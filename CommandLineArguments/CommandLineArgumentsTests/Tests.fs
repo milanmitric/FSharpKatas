418553d89dd814b6bc36773d6707e6071948370c
Milan Mitric <milan_442@hotmail.com>
2020-01-28 00:09:24 +0100

    [CommandLineArguments] Added non recursive implementation of parsing.

--- CommandLineArguments/CommandLineArgumentsTests/Tests.fs
+++ CommandLineArguments/CommandLineArgumentsTests/Tests.fs
@@ -6,7 +6,32 @@ open FsUnit.Xunit
 open Xunit
 open Parser
 
+let flagsOrderByName = {verbose = VerboseOutput; subdirectories = IncludeSubdirectories; orderby = OrderByName}
+let flagsOrderBySize = {verbose = VerboseOutput; subdirectories = IncludeSubdirectories; orderby = OrderBySize}
 [<Fact>]
-let ``Parse verbose flag`` () =
-    "MyApp /V" |> parse |> should equal {noOptions with verbose = true }
+let ``Parse verbose flag``() =
+    "MyApp /V" |> parseCommandLine |> should equal { defaultOptions with verbose = VerboseOutput }
+
+[<Fact>]
+let ``Parse subdirectories flag``() =
+    "MyApp /S" |> parseCommandLine |> should equal { defaultOptions with subdirectories = IncludeSubdirectories }
+
+[<Fact>]
+let ``Parse order by name``() =
+    "MyApp /O N" |> parseCommandLine |> should equal { defaultOptions with orderby = OrderByName }
+
+[<Fact>]
+let ``Parse order by size``() =
+    "MyApp /O S" |> parseCommandLine |> should equal { defaultOptions with orderby = OrderBySize }
     
+[<Fact>]
+let ``Parse order by invalid switch``() =
+    "MyApp /O a" |> parseCommandLine |> should equal defaultOptions
+
+[<Fact>]
+let ``Parse flags with order by name`` () =
+    "MyApp /V /S /O N" |> parseCommandLine |> should equal flagsOrderByName
+    
+[<Fact>]
+let ``Parse flags with order by size`` () =
+    "MyApp /V /S /O S" |> parseCommandLine |> should equal flagsOrderBySize
--- CommandLineArguments/Parser.fs
+++ CommandLineArguments/Parser.fs
@@ -1,31 +1,89 @@
 ﻿namespace CommandLineArguments
-open Microsoft.VisualBasic
-open System
 
 module Parser =
 
-    let OrderByName = "N"
-    let OrderBySize = "S"
+    type OrderByOption = | OrderBySize | OrderByName
+    type SubdirectoryOption = | IncludeSubdirectories | ExcludeSubdirectories
+    type VerboseOption = | VerboseOutput | TerseOutput
+
     type CommandLineOptions = {
-        verbose: bool;
-        subdirectories: bool;
-        orderby: string;
-    }
+        verbose: VerboseOption;
+        subdirectories: SubdirectoryOption;
+        orderby: OrderByOption
+        }
 
-    let noOptions = { verbose = false; subdirectories = false; orderby = "" }
+    type ParseMode = | TopLevel | OrderBy
 
-    let rec private  parseInternal (optionsSoFar: CommandLineOptions) (input: string list) : CommandLineOptions =
-        match input with
-        | [] -> optionsSoFar
-        | "/V"::rest ->
-            let newOptions = { optionsSoFar with verbose = true}
-            parseInternal newOptions rest
-        | x::rest ->
-            eprintf "Unrecognized switch %s" x
-            parseInternal optionsSoFar rest
+    type FoldState = {
+        options: CommandLineOptions;
+        parseMode: ParseMode;
+        }
 
-    let parse (input: string) =
-        let appWithArgs = input.Split(" ") |> Array.toList 
+    let defaultOptions = {
+            verbose = TerseOutput;
+            subdirectories = ExcludeSubdirectories;
+            orderby = OrderByName
+            }
+
+    // parse the top-level arguments
+    // return a new FoldState
+    let parseTopLevel arg optionsSoFar =
+        match arg with
+
+        // match verbose flag
+        | "/V" ->
+            let newOptionsSoFar = { optionsSoFar with verbose = VerboseOutput }
+            { options = newOptionsSoFar; parseMode = TopLevel }
+
+        // match subdirectories flag
+        | "/S" ->
+            let newOptionsSoFar = { optionsSoFar with subdirectories = IncludeSubdirectories }
+            { options = newOptionsSoFar; parseMode = TopLevel }
+
+        // match sort order flag
+        | "/O" ->
+            { options = optionsSoFar; parseMode = OrderBy }
+
+        // handle unrecognized option and keep looping
+        | x ->
+            printfn "Option '%s' is unrecognized" x
+            { options = optionsSoFar; parseMode = TopLevel }
+
+    // parse the orderBy arguments
+    // return a new FoldState
+    let parseOrderBy arg optionsSoFar =
+        match arg with
+        | "S" ->
+            let newOptionsSoFar = { optionsSoFar with orderby = OrderBySize }
+            { options = newOptionsSoFar; parseMode = TopLevel }
+        | "N" ->
+            let newOptionsSoFar = { optionsSoFar with orderby = OrderByName }
+            { options = newOptionsSoFar; parseMode = TopLevel }
+        // handle unrecognized option and keep looping
+        | _ ->
+            printfn "OrderBy needs a second argument"
+            { options = optionsSoFar; parseMode = TopLevel }
+
+    // create a helper fold function
+    let foldFunction state element =
+        match state with
+        | { options = optionsSoFar; parseMode = TopLevel } ->
+            // return new state
+            parseTopLevel element optionsSoFar
+
+        | { options = optionsSoFar; parseMode = OrderBy } ->
+            // return new state
+            parseOrderBy element optionsSoFar
+
+    // create the "public" parse function
+    let parseCommandLine (commandLineArgs: string) =
+
+        let initialFoldState =
+            { options = defaultOptions; parseMode = TopLevel }
+
+        let appWithArgs = commandLineArgs.Split(" ") |> Array.toList
         match appWithArgs with
-        | [] -> noOptions
-        | app::args -> parseInternal noOptions args
+        | [] -> defaultOptions
+        | app :: args ->
+            let resultState = args |> List.fold foldFunction initialFoldState
+            resultState.options
